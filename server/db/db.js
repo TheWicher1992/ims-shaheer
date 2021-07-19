2398ed79e732e51cc626a6453fa899afd5bb96ce
Muhammad Sameer Nadeem <sameer.nadeem24@gmail.com>
2021-07-20 01:55:39 +0500

    Database Connection Set Up

---
+++ server/db/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const config = require('config')
+
+
+module.exports = () => {
+    const dbUri = config.get('db')
+
+
+    mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+        if (err) {
+            console.error("Error connection to database!", err.message)
+        }
+        else {
+            console.log("Database connected successfuly!")
+        }
+    })
+}
--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const PORT = process.env.PORT || 5000
-
+const connectDB = require('./db/db')
 app.use(cors())
 app.use(express.json())
+
+connectDB()
 
 app.get('/', (req, res) => res.send("Hello World!"))
 
