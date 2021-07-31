0bc586f02f4fca49bc09d5a2b5595fdaf36639c6
Muhammad Sameer Nadeem <sameer.nadeem24@gmail.com>
2021-07-31 21:27:26 +0500

    get Prod by ID

--- server/routes/product.js
+++ server/routes/product.js
@@ -42,8 +42,15 @@ router.post('/', async (req, res) => {
     })
 })
 
-router.get('/:id', (req, res) => {
-    res.send(`Post with ID ${req.params.id}`)
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+
+    const product = await Product.findById(id)
+
+    return res.status(200).json({
+        product
+    })
+
 })
 
 router.get('/:page', async (req, res) => {
