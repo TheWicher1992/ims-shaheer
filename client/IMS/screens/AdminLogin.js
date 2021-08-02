79a50adea4c3716b15d351f1b382a2feb92d5dd6
unknown <22100145@lums.edu.pk>
2021-08-02 22:41:31 +0500

    keyboard doesnt cover the input containers/fields

--- client/IMS/screens/AdminLogin.js
+++ client/IMS/screens/AdminLogin.js
@@ -1,60 +1,62 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button, Dimensions, TextInput, TouchableOpacity } from 'react-native'; 
+import { StyleSheet, Text, View, Button, Dimensions, TextInput, TouchableOpacity, KeyboardAvoidingView } from 'react-native'; 
 
 
 const AdminLogin = props => {
     return(
-        <View>
-            <View style={styles.circleNumber1}>
-
-            </View>
-            <View style={styles.screen}>
-                <View>
-                    <Text style={styles.title}>LOGIN</Text>
-                </View>
-                        
-                <View>
-                    <Text style={styles.subtitle}>Sign in to your account</Text>
-                </View>
-            </View>
-                <View style={styles.circleNumber2}>
-                    
-                    
-                </View>
-                <View style={styles.circleNumber3}>
+        <KeyboardAvoidingView style = {styles.containerView} behavior = "padding">
+            <View>
+                <View style={styles.circleNumber1}>
 
                 </View>
-                <View style={styles.container}>
-                    <TextInput style={styles.input} placeholder="Email" autoCorrect={false}/>
-                    <TextInput style={styles.input} placeholder="Password" secureTextEntry={true} autoCorrect={false}/>
-                    <View >
-                        <TouchableOpacity onPress={() => props.navigation.navigate({routeName: 'Dashboard'})}>
-                            <View style={styles.buttonContainer}>
-                                <Text style={styles.buttonText}>Login</Text>
-                            </View>
-                            </TouchableOpacity>
+                <View style={styles.screen}>
+                    <View>
+                        <Text style={styles.title}>LOGIN</Text>
                     </View>
-                        
+                            
+                    <View>
+                        <Text style={styles.subtitle}>Sign in to your account</Text>
+                    </View>
                 </View>
-                <View style={styles.circleNumber4}>
+                    <View style={styles.circleNumber2}>
+                        
+                        
+                    </View>
+                    <View style={styles.circleNumber3}>
 
-                </View>
-                <View style={styles.circleNumber5}>
-                    
-                </View>
-                <View style={styles.container}>
-                    {Dimensions.get('window').height === 1232 ? <Text style={styles.footer}>Zaki Sons</Text> : console.log(Dimensions.get('window').height)}
-                </View>
+                    </View>
+                    <View style={styles.container}>
+                        <TextInput style={styles.input} placeholder="Email" autoCorrect={false}/>
+                        <TextInput style={styles.input} placeholder="Password" secureTextEntry={true} autoCorrect={false}/>
+                        <View >
+                            <TouchableOpacity onPress={() => props.navigation.navigate({routeName: 'Dashboard'})}>
+                                <View style={styles.buttonContainer}>
+                                    <Text style={styles.buttonText}>Login</Text>
+                                </View>
+                                </TouchableOpacity>
+                        </View>
+                            
+                    </View>
+                    <View style={styles.circleNumber4}>
+
+                    </View>
+                    <View style={styles.circleNumber5}>
                         
-                        
+                    </View>
+                    <View style={styles.container}>
+                        {Dimensions.get('window').height === 1232 ? <Text style={styles.footer}>Zaki Sons</Text> : console.log(Dimensions.get('window').height)}
+                    </View>
                             
                             
                                 
-            
-            
+                                
+                                    
+                
+                
 
-        
-        </View>
+            
+            </View>
+        </KeyboardAvoidingView>
         
         
     )
