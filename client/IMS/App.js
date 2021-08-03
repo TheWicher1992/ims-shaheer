bcad122eaa840e84693e238718f7657de9978843
TheWicher1992 <sameer.nadeem24@gmail.com>
2021-08-03 17:24:20 +0500

    Added Redux

--- client/IMS/App.js
+++ client/IMS/App.js
@@ -2,14 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AppNavigation from './navigation/AppNavigation';
+import { Provider } from 'react-redux'
+import store from "./store";
 
 export default function App() {
   return (
+
+    <Provider store={store}>
+
     // <View style={styles.container}>
     //   <Text>Open up App.js to start working on your app!</Text>
     //   <StatusBar style="auto" />
     // </View>
-    <AppNavigation />
+      <AppNavigation />
+
+    </Provider>
+
   );
 }
 
