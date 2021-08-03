2c1c0abfd4c563887eea95d8fa0ceeb04d9ce537
Muhammad Sameer Nadeem <sameer.nadeem24@gmail.com>
2021-08-03 19:44:12 +0500

    Make Purchase Route

--- server/routes/setRoutes.js
+++ server/routes/setRoutes.js
@@ -1,6 +1,8 @@
 const authRouter = require('./auth')
 const productRouter = require('./product')
+const purchaseRouter = require('./purchase')
 module.exports = function (app) {
     app.use('/api/auth', authRouter)
     app.use('/api/product', productRouter)
+    app.use('/api/purchase', purchaseRouter)
 }
