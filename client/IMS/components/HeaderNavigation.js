a78a0a6005f1c9dbecf4299352a13996a49c3f92
raahem <raahemasghar97@gmail.com>
2021-08-06 01:28:40 +0500

    userInfo modal styling complete

--- client/IMS/components/HeaderNavigation.js
+++ client/IMS/components/HeaderNavigation.js
@@ -3,13 +3,16 @@ import UserInfoModal from './UserInfoModal';
 import HeaderButton from '../components/HeaderButton';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { EvilIcons } from '@expo/vector-icons';
+import { TouchableOpacity } from "react-native";
+
 const HeaderNavigation = props => {
     const [ setModal, setModalState ] = useState(false)
     return(
-        <HeaderButtons HeaderButtonComponent = {HeaderButton}>
-            <EvilIcons name={"user"} size={36} color = {"white"} style = {{right: 10}} onPress = {() => {setModalState(true)}} />
-            {setModal===true ? <UserInfoModal /> : null}
+        <TouchableOpacity><HeaderButtons HeaderButtonComponent = {HeaderButton}>
+            <EvilIcons name={"user"} size={36} color = {"white"} style = {{right: 10}} onPress = {() => {console.log('yo'),setModalState(true)}} />
+            {setModal===true ? <UserInfoModal state={true}/> : null}
         </HeaderButtons>
+        </TouchableOpacity> 
     )
 }
 
