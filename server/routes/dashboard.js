0fd29b738f11d15b6eb28a4acb3a44d97e9b4b90
Haseeb Abid <haseebabid@hotmail.com>
2021-08-07 23:37:47 +0500

    dashboard stats

---
+++ server/routes/dashboard.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const router = express.Router()
+const Product = require('../models/Product')
+const Sale = require('../models/Sale')
+const config = require('config')
+
+router.get('/outOfStock', async (req, res) => {
+    try {
+
+
+        const outOfStock = await Product.count({
+            stock : 0 
+        })
+
+        return res.status(200).json({
+            outOfStock
+        })
+    }
+    catch (err) {
+        console.log(err)
+        return res.status(400).json({
+            error: 'SERVER_ERROR'
+        })
+    }
+})
+
+router.get('/todayRevenue', async (req, res) => {
+    try {
+
+        var start = new Date();
+        start.setHours(0,0,0,0);
+
+        var end = new Date();
+        end.setHours(23,59,59,999)
+        const todayRevenue = await Sale.aggregate([
+        {$match : {date: {$gte: start, $lt: end}}},
+        {$group :{
+            _id : null,
+            revenue : {$sum : "$total"}}}
+        ])
+
+        return res.status(200).json({
+            todayRevenue
+        })
+    }
+    catch (err) {
+        console.log(err)
+        return res.status(400).json({
+            error: 'SERVER_ERROR'
+        })
+    }
+})
+
+router.get('/pendingDeliveries', async (req, res) => {
+    try {
+
+
+        const pendingDeliveries = await Sale.count({
+            deliveryStatus: false 
+        })
+
+        return res.status(200).json({
+            pendingDeliveries
+        })
+    }
+    catch (err) {
+        return res.status(400).json({
+            error: 'SERVER_ERROR'
+        })
+    }
+})
+
+module.exports = router
--- server/routes/setRoutes.js
+++ server/routes/setRoutes.js
@@ -2,9 +2,11 @@ const authRouter = require('./auth')
 const productRouter = require('./product')
 const warehouseRouter = require('./warehouses')
 const clientRouter = require('./client')
+const dashRouter = require('./dashboard')
 module.exports = function (app) {
     app.use('/api/auth', authRouter)
     app.use('/api/product', productRouter)
     app.use('/api/warehouse', warehouseRouter)
     app.use('/api/client', clientRouter)
+    app.use('/api/dashboard', dashRouter)
 }
