730ecba9c59d055c18205a238ad93f036f407139
ali <aleehassan1999@gmil.com>
2021-08-10 14:30:50 +0500

    backend done from my end

--- server/routes/setRoutes.js
+++ server/routes/setRoutes.js
@@ -1,6 +1,13 @@
 const authRouter = require('./auth')
 const productRouter = require('./product')
+const orderRouter = require('./order')
+const saleRouter = require('./sale')
+const supplierRouter = require('./supplier')
 module.exports = function (app) {
     app.use('/api/auth', authRouter)
     app.use('/api/product', productRouter)
+    app.use('/api/order',orderRouter)
+    
+    app.use('/api/supplier',supplierRouter)
+    app.use('/api/sale',saleRouter)
 }
