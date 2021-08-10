8a861c1ca73a3710071767054d4e602b15004db2
TheWicher1992 <sameer.nadeem24@gmail.com>
2021-08-10 22:08:19 +0500

    Multiple Warehouse for a Stock // DeliverOrder Bug Here

--- server/routes/purchase.js
+++ server/routes/purchase.js
@@ -4,17 +4,18 @@ const Purchase = require('../models/Purchase')
 const Product = require('../models/Product')
 const DeliveryOrder = require('../models/DeliveryOrder')
 const Warehouse = require('../models/Warehouse')
-
+const Stock = require('../models/Stock')
 router.post('/', async (req, res) => {
     const {
-        productID,
+        product,
         quantity,
         client,
         payment,
         total,
         note,
         isDeliveryOrder,
-        location
+        location,
+        warehouseID
     } = req.body
 
     const purchase = new Purchase({
@@ -25,13 +26,6 @@ router.post('/', async (req, res) => {
         total,
         note
     })
-
-
-    //Update product stock
-    const product = await Product.findOne({
-        _id: productID
-    })
-    product.stock += quantity
 
     if (isDeliveryOrder) {
         const deliveryOrder = new DeliveryOrder({
@@ -45,15 +39,28 @@ router.post('/', async (req, res) => {
     }
     else {
         const warehouse = await Warehouse.findOne({
-            _id: product.warehouse
+            _id: warehouseID
         })
+        const stock = await Stock.findOne({
+            warehouse: warehouseID,
+            product
+        })
+
+        if (!stock) {
+            await new Stock({
+                product,
+                warehouse: warehouseID,
+                stock: quantity
+            }).save()
+        }
+        else {
+            stock.stock += quantity
+        }
         warehouse.totalStock += quantity
-        product.physicalStock += quantity
         await warehouse.save()
     }
 
     await purchase.save()
-    await product.save()
 
     return res.status(200).json({
         purchase
@@ -63,6 +70,72 @@ router.post('/', async (req, res) => {
 
 })
 
+// router.put('/:id', async (req, res) => {
 
+//     const {
+//         product,
+//         quantity,
+//         client,
+//         payment,
+//         total,
+//         note,
+//         isDeliveryOrder,
+//         location,
+//         warehouseID
+//     } = req.body
+
+//     const purchase = await Purchase.findOne({
+//         _id: req.params.id
+//     })
+
+//     purchase.product = product
+//     purchase.quantity = quantity
+//     purchase.client = client
+//     purchase.payment = payment
+//     purchase.total = total
+//     purchase.note = note
+
+
+//     if (isDeliveryOrder) {
+//         const deliveryOrder = new DeliveryOrder({
+//             client,
+//             product,
+//             quantity,
+//             location,
+//             note
+//         })
+//         await deliveryOrder.save()
+//     }
+//     else {
+//         const warehouse = await Warehouse.findOne({
+//             _id: warehouseID
+//         })
+//         const stock = await Stock.findOne({
+//             warehouse: warehouseID,
+//             product
+//         })
+
+//         if (!stock) {
+//             await new Stock({
+//                 product,
+//                 warehouse: warehouseID,
+//                 stock: quantity
+//             }).save()
+//         }
+//         else {
+//             stock.stock += quantity
+//         }
+//         warehouse.totalStock += quantity
+//         await warehouse.save()
+//     }
+
+//     await purchase.save()
+
+//     return res.status(200).json({
+//         purchase
+//     })
+
+
+// })
 
 module.exports = router
