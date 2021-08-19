91c76f401a704ffcb2bd7a1b4ff95bffe74fd7f2
Haseeb Abid <haseebabid@hotmail.com>
2021-08-19 22:49:36 +0500

    user card

--- client/IMS/components/HeaderNavigation.js
+++ client/IMS/components/HeaderNavigation.js
@@ -9,7 +9,8 @@ const HeaderNavigation = props => {
     const [ setModal, setModalState ] = useState(false)
     const handleClose = ()=>{
         setModalState(false)
-      }
+    }
+    
     return(
         <TouchableOpacity><HeaderButtons HeaderButtonComponent = {HeaderButton}>
             <EvilIcons name={"user"} size={36} color = {"white"} style = {{right: 10}} onPress = {() => setModalState(true)} />
