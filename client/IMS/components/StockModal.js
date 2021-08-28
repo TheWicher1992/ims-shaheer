94c9d571e16db366c9f99dced314b30d4c9890c2
RaahemAsghar <raahemasghar97@gmail.com>
2021-08-28 20:38:03 +0500

    employee side onTouchOutside

--- client/IMS/components/StockModal.js
+++ client/IMS/components/StockModal.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Alert, Modal, StyleSheet, Text, View, TouchableOpacity, Dimensions, KeyboardAvoidingView, ScrollView } from "react-native";
+import { Alert, Modal, StyleSheet, Text, View, TouchableOpacity, Dimensions, KeyboardAvoidingView, ScrollView, TouchableWithoutFeedback } from "react-native";
 import ShiftWarehouseModal from "./ShiftWarehouseModal";
 
 const StockModal = props => {
@@ -29,6 +29,9 @@ const StockModal = props => {
             props.handleClose();
           }}
         >
+          <TouchableWithoutFeedback onPress={() => props.handleClose()}>
+            <View style={styles.modalOverlay} />
+          </TouchableWithoutFeedback>
           <View style={styles.centeredView}>
             {/* {console.log("printing object ", props.object)} */}
             <View style={styles.modalView}>
@@ -150,6 +153,13 @@ const styles = StyleSheet.create({
     //elevation: 5,
     width: '80%',
     height: Dimensions.get('window').height > 900 ? '65%' : Dimensions.get('window').height * 0.60
+  },
+  modalOverlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
   },
 
 });
